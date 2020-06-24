1293fbb4ecfcf9dda83d80fc2d8d103ad60a5de2
Ryan Brazeal @ UFL <ryan.brazeal@ufl.edu>
2020-06-24 10:12:04 -0600

    comment updates

--- intervalometer/openmms_intervalometer.ino
+++ intervalometer/openmms_intervalometer.ino
@@ -1,9 +1,36 @@
-//Project: Camera Intervalometer Firmware for OpenMMS
-//By: Ryan Brazeal (ryan.brazeal@ufl.edu)
-//Started: July 2018
-//Updates: 
-//    April 2019 - Removed 100msec from each of the final delay commands in order to account for the ~100msec of internal processing time needed
-//    May 2020 - Updated intervals
+/*##########################################################################
+#                  OpenMMS Camera Intervalometer Firmware                  #
+############################################################################
+# Version: 1.3                                                             #
+# Date:    June 2020                                                       #
+# Author:  Ryan Brazeal                                                    #
+# Email:   ryan.brazeal@ufl.edu                                            #
+#                                                                          #
+#    OPEN SOURCE LICENSE INFO:                                             #
+#                                                                          #
+#    This file is part of OpenMMS_OSS.                                     #
+#                                                                          #
+#    OpenMMS_OSS is free software: you can redistribute it and/or modify   #
+#    it under the terms of the GNU General Public License as published by  #
+#    the Free Software Foundation, either version 3 of the License, or     #
+#    (at your option) any later version.                                   #
+#                                                                          #
+#    OpenMMS_OSS is distributed in the hope that it will be useful,        #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#    GNU General Public License for more details.                          #
+#                                                                          #
+#    You should have received a copy of the GNU General Public License     #
+#    along with OpenMMS_OSS. If not, see <https://www.gnu.org/licenses/>.  #
+#                                                                          #
+############################################################################
+# Updates:                                                                 #
+#    April 2019 - Removed 100msec from each of the final delay commands in #
+#                 order to account for the ~100msec of internal processing #
+#                 time needed                                              #
+#                                                                          #
+#    May 2020 - Updated intervals                                          #
+##########################################################################*/
 
 int inputPin1 = 9;
 int inputPin2 = 10;
